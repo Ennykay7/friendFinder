94c0da93d9e671587c1758b723f50111abbf1d1f
Kenny <ksperkins.ksp@gmail.com>
2018-09-25 00:30:47 -0700

    commit

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+var friendsList = [
+    {
+      name: "Ken",
+      photo: "http://www.personalbrandingblog.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640-300x300.png",
+      scores: [1,1,1,1,1]
+    }
+  ];
+  
+  module.exports = friendsList;
